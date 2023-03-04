ee38de8545f0d7749600b647163423f94ad43f38
Olesa0896 <olesa600@gmail.com>
2023-03-04 10:02:13 -0500

    ADD: install bootsrap and create component directory with todoForm and todoList

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const App = () => {
   return (
-    <>
-      <h1>Soy el App</h1>
-      <p> Soy un parrafo</p>
-    </>
+    <div className='container mt-4'>
+      <div className='row'>
+        <div className='col-8'>
+          <h1>Lista de tareas</h1>
+        </div>
+        <div className='col-4'>
+          <h1>Formulario</h1>
+        </div>
+
+      </div>
+    </div>
   );
 }
 
