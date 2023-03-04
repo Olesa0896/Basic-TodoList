d00c1ba6642459f96fb45e6908947f10cb9b0062
Olesa0896 <olesa600@gmail.com>
2023-03-04 13:03:04 -0500

    ADD: onChange listener to the input and textarea and capture the respective value

--- todo-app/src/components/TodoForm.js
+++ todo-app/src/components/TodoForm.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 
 const TodoForm = () => {
+  const initialFormValues = {
+    title: '',
+    description: ''
+  }
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const { title, description } = formValues;
+
+  const handleInputChange = (e) => {
+
+    const changedFormValues = {
+      ...formValues, [e.target.name]: e.target.value
+    }
+    setFormValues(changedFormValues)
+  }
   return (
     <div>
       <h1>Nueva Tarea</h1>
@@ -9,10 +23,16 @@ const TodoForm = () => {
           type='text'
           placeholder='Título'
           className='form-control'
+          value={title}
+          name='title'
+          onChange={handleInputChange}
         />
         <textarea
           placeholder='Descripción'
           className='form-control mt-2'
+          value={description}
+          name='description'
+          onChange={handleInputChange}
         ></textarea>
         <button
           className='btn btn-primary w-100 mt-2'
