5198820f8ca5c1462118acf85c9412ec847c8aa4
Olesa0896 <olesa600@gmail.com>
2023-03-04 14:39:49 -0500

    ADD: an alert when theres is no task

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -7,16 +7,25 @@ const TodoList = ({ todos, todoDelete, todoToogleCompleted, setTodoEdit }) => {
   return (
     <div>
       <h1>Soy TodoList</h1>
+
       {
-        todos.map(todo => (
-          <Todo
-            todo={todo}
-            key={todo.id}
-            todoDelete={todoDelete}
-            todoToogleCompleted={todoToogleCompleted}
-            setTodoEdit={setTodoEdit}
-          />
-        ))
+        todos.length === 0
+          ? (
+            <div className='alert alert-primary'>
+              No hay tareas, Por favor agregar una nueva tarea🤓
+          </div>
+          )
+          : (
+            todos.map(todo => (
+              <Todo
+                todo={todo}
+                key={todo.id}
+                todoDelete={todoDelete}
+                todoToogleCompleted={todoToogleCompleted}
+                setTodoEdit={setTodoEdit}
+              />
+            ))
+          )
       }
     </div>
   );
