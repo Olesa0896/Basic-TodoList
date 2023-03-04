5fc1a26a83ced91d04451a43fe4ecd0a45dbdfd2
Olesa0896 <olesa600@gmail.com>
2023-03-04 11:43:05 -0500

    ADD: move the to do card to an external component

---
+++ todo-app/src/components/Todo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Todo = () => {
+  return (
+    <div className='card mt-2'>
+      <div className='card-body'>
+        <h3 className='card-title text-end'>
+          Título de la tarea
+          <button className='btn btn-sm btn-outline-success ms-2'> Terminar </button>
+        </h3>
+        <p className='card-text text-end'>
+          Descripción de la tarea
+        </p>
+        <hr />
+        <div className='d-flex justify-content-end'>
+          <button className='btn btn-sm btn-outline-primary me-2'>Editar</button>
+          <button className='btn btn-sm btn-outline-danger'>Eliminar</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Todo;
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,25 +1,12 @@
 import React from 'react';
+import Todo from './Todo';
 
 const TodoList = () => {
   return (
     <div>
       <h1>Soy TodoList</h1>
-      <div className='card'>
-        <div className='card-body'>
-          <h3 className='card-title text-end'>
-            Título de la tarea
-            <button className='btn btn-sm btn-outline-success ms-2'> Terminar </button>
-          </h3>
-          <p className='card-text text-end'>
-            Descripción de la tarea
-          </p>
-          <hr />
-          <div className='d-flex justify-content-end'>
-            <button className='btn btn-sm btn-outline-primary me-2'>Editar</button>
-            <button className='btn btn-sm btn-outline-danger'>Eliminar</button>
-          </div>
-        </div>
-      </div>
+      <Todo />
+      <Todo />
     </div>
   );
 }
